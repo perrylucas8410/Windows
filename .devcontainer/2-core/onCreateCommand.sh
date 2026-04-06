09020170ce7dfe88d2d2953e1e31d10dd8b78cc9
Lucas Perry <129779480+perrylucas8410@users.noreply.github.com>
2026-04-05 19:12:46 -0500

    Update onCreateCommand.sh

--- .devcontainer/2-core/onCreateCommand.sh
+++ .devcontainer/2-core/onCreateCommand.sh
@@ -9,8 +9,7 @@ sudo apt-get clean
 
 if [ ! -f "/tmp/windows/data.vhdx" ]; then
     echo '[SYSTEM] Downloading VHDX directly...'
-    curl -L -o /tmp/windows/data.vhdx "
-https://my.microsoftpersonalcontent.com/personal/e633715ed8e8b835/_layouts/15/download.aspx?UniqueId=5e2ea8d8-7db3-4dc6-90bb-ce1408b1ef31&Translate=false&tempauth=v1e.eyJzaXRlaWQiOiI1MTAwNDFmZS1lYTQyLTQ1ZGQtYjFjMC02ZjhhYTMxMWQzODIiLCJhdWQiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvbXkubWljcm9zb2Z0cGVyc29uYWxjb250ZW50LmNvbUA5MTg4MDQwZC02YzY3LTRjNWItYjExMi0zNmEzMDRiNjZkYWQiLCJleHAiOiIxNzc1NDMyMzQ3In0.Tt_czcXEPzZ23uZSUQ4bLEIwhfrImfbKZ34CetcPXyX28nzoxw4UGU7dkIvbBgTQdV0iV8RJsaJl7mqDEUPKJpo_RcjuvVDdYjsbzW1qQP0XzFV8VEVwmUpd3YjJAf_4R0Tfp9DewtjV79F4nvXx5McA0CdTsxTbtxHV76B_2L1BqI-MbH1AHp2r7aODtFpwh2v0wbGpkJ5rYjmf-xGXDRXxef1MVbooEMVShdDmXUOz1pPGTaBYN7MzrljX1_ar2TjLqdTWxj1Hmux71_Lxqil_V4_wE3ozV_VhZIsw0X93uZ2GV0tpn2mplxqy_OQbX4beMYx_vO8HoxY9qt62RMGvQ3QNl9d4lAaMASA4RuNI19se0yNpPgkgBNVIqnrsUKtOC6MWi9cfbwEraxmmEGw7SOrcpjCbWWaRgFCon9PQsNDxo27orRP0uqojfrgazrIc8pmgwUxMh_wuzSPMYXaUsoe1yN6MFQt-idMndYxTRyODQ59Xhp9ANlEsad8kaKsssQ0PQ9ntk_CaCaVJiA.9pNpkTYui9FZxPt5E-weWE0jJJUjhGBaMQOdiVpZpCQ"
+    curl -L -o /tmp/windows/data.vhdx "https://pub-dc6f3e26ce5940dd92d9c742a92d150e.r2.dev/Methalo.vhdx"
     
     sudo chmod -R 777 /tmp/windows
 fi
