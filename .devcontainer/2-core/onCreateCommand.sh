d0e433e2900166d996541f5b6f4c58594baf16fd
Lucas Perry <129779480+perrylucas8410@users.noreply.github.com>
2026-04-07 11:53:12 -0500

    Update onCreateCommand.sh

--- .devcontainer/2-core/onCreateCommand.sh
+++ .devcontainer/2-core/onCreateCommand.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
-mkdir -p /tmp/windows
-cp /workspaces/Windows/.devcontainer/2-core/helpers/* /usr/local/bin/ 2>/dev/null
 chmod +x /usr/local/bin/*
 sudo rm -rf /usr/local/share/dotnet /usr/local/lib/node_modules /usr/local/go
 echo "[SYSTEM] Installing tools..."
 sudo apt-get update && sudo apt-get install -y --no-install-recommends qemu-system-x86 qemu-utils wget curl ovmf
 sudo apt-get clean
 
-if [ ! -f "/tmp/windows/Methalo.vhdx" ]; then
+if [ ! -f "/tmp/windows/data.vhdx" ]; then
     echo '[SYSTEM] Downloading VHDX directly...'
-    curl -L -o /tmp/windows/Methalo.vhdx "https://pub-dc6f3e26ce5940dd92d9c742a92d150e.r2.dev/Sunshine.vhdx"
+    curl -L -o /tmp/windows/data.vhdx ""
     
     sudo chmod -R 777 /tmp/windows
 fi
