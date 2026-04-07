c87774a5c2e3e28228fb7fbaafe092631fc0b31f
Lucas Perry <129779480+perrylucas8410@users.noreply.github.com>
2026-04-07 00:52:58 -0500

    Update onCreateCommand.sh

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p /Workspaces/Windows/Windows
+mkdir -p /tmp/windows
 cp /workspaces/Windows/.devcontainer/helpers/* /usr/local/bin/ 2>/dev/null
 chmod +x /usr/local/bin/*
 sudo rm -rf /usr/local/share/dotnet /usr/local/lib/node_modules /usr/local/go
@@ -7,10 +7,10 @@ echo "[SYSTEM] Installing tools..."
 sudo apt-get update && sudo apt-get install -y --no-install-recommends qemu-system-x86 qemu-utils wget curl ovmf
 sudo apt-get clean
 
-if [ ! -f "/Workspaces/Windows/Windows/Methalo.vhdx" ]; then
+if [ ! -f "/tmp/windows/Methalo.vhdx" ]; then
     echo '[SYSTEM] Downloading VHDX directly...'
-    curl -L -o /Workspaces/Windows/Windows/Methalo.vhdx "https://pub-dc6f3e26ce5940dd92d9c742a92d150e.r2.dev/Sunshine.vhdx"
+    curl -L -o /tmp/windows/Methalo.vhdx "https://pub-dc6f3e26ce5940dd92d9c742a92d150e.r2.dev/Sunshine.vhdx"
     
-    sudo chmod -R 777 /Workspaces/Windows/Windows
+    sudo chmod -R 777 /tmp/windows
 fi
 echo '[SUCCESS] Setup finished. Type start.'
