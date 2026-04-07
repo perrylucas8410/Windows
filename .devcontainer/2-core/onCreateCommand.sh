3a59856fc3448e76c9658b09f1f0e3fd5173c63f
Lucas Perry <129779480+perrylucas8410@users.noreply.github.com>
2026-04-07 11:57:41 -0500

    Update onCreateCommand.sh

--- .devcontainer/2-core/onCreateCommand.sh
+++ .devcontainer/2-core/onCreateCommand.sh
@@ -1,15 +1,15 @@
+#!/bin/bash
 mkdir -p /tmp/windows
-cp /workspaces/Windows/.devcontainer/2-core/helpers/* /usr/local/bin/ 2>/dev/null
+cp /workspaces/Windows/.devcontainer/4-core/helpers/* /usr/local/bin/ 2>/dev/null
 chmod +x /usr/local/bin/*
 sudo rm -rf /usr/local/share/dotnet /usr/local/lib/node_modules /usr/local/go
 echo "[SYSTEM] Installing tools..."
-sudo apt-get update && sudo apt-get install -y --no-install-recommends qemu-system-x86 qemu-utils wget curl ovmf
+sudo apt-get update && sudo apt-get install -y --no-install-recommends qemu-system-x86 qemu-utils wget curl novnc websockify net-tools ovmf
 sudo apt-get clean
 
 if [ ! -f "/tmp/windows/data.vhdx" ]; then
     echo '[SYSTEM] Downloading VHDX directly...'
     curl -L -o /tmp/windows/data.vhdx ""
-    
     sudo chmod -R 777 /tmp/windows
 fi
 echo '[SUCCESS] Setup finished. Type start.'
