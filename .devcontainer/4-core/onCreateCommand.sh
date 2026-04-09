f79b5a1f297c4be76ced7ca16bbf6b9c92176552
lucasperry1111 <lucasperry1111@gmail.com>
2026-04-09 18:29:07 -0500

    Update onCreateCommand.sh

--- .devcontainer/4-core/onCreateCommand.sh
+++ .devcontainer/4-core/onCreateCommand.sh
@@ -2,22 +2,21 @@ mkdir -p /tmp/windows
 cp /workspaces/Windows/.devcontainer/4-core/helpers/* /usr/local/bin/ 2>/dev/null
 chmod +x /usr/local/bin/*
 
+echo "[SYSTEM] Installing tools..."
 sudo apt-get update && sudo apt-get install -y --no-install-recommends qemu-system-x86 qemu-utils wget curl novnc websockify net-tools ovmf stunnel4
 sudo apt-get clean
 
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/stunnel/stunnel.key -out /etc/stunnel/stunnel.crt -subj "/C=US/ST=State/L=City/O=Organization/CN=localhost"
-sudo cat > /etc/stunnel/myrtille.conf <<EOF
-pid = /var/run/stunnel4/myrtille.pid
+sudo bash -c 'cat > /etc/stunnel/myrtille.conf <<EOF
+pid = /tmp/stunnel.pid
 [myrtille]
 accept = 443
 connect = 127.0.0.1:8080
 cert = /etc/stunnel/stunnel.crt
 key = /etc/stunnel/stunnel.key
-EOF
+EOF'
 
-if [ ! -f "/tmp/windows/methalo.vhdx" ]; then
-    echo '[SYSTEM] Downloading Windows VHDX...'
-    curl -L -o /tmp/windows/methalo.vhdx "https://pub-dc6f3e26ce5940dd92d9c742a92d150e.r2.dev/methalo.vhdx"
-    sudo chmod -R 777 /tmp/windows
-fi
-echo '[SUCCESS] Setup finished.'
+echo '----------------------------------------------------'
+echo '[SUCCESS] SETUP COMPLETE!'
+echo 'Type "start" to download and boot Windows.'
+echo '----------------------------------------------------'
